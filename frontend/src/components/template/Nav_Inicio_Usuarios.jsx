1c3868882f8ff0d47b2af874a6ea36162c775864
Samuel Fernandes Silva <169082016+Samux9221@users.noreply.github.com>
2024-09-28 13:18:19 -0600

    ...
    ....

--- frontend/src/components/template/Nav_Inicio_Usuarios.jsx
+++ frontend/src/components/template/Nav_Inicio_Usuarios.jsx
@@ -1,47 +1,10 @@
-/*import React from 'react'
-
-export default props =>
-    <aside>
-        <a href="#/">
-            <i className="fa fa-home"></i> Início
-        </a>
-            <li style={{ color: 'white', textDecoration: 'none', listStyleType: 'none' }}>
-                <a href="#/users">
-                    <i className="fa fa-users"></i> Usuários
-                </a>
-                <ul>
-                    <li style={{ color: 'white', textDecoration: 'none', listStyleType: 'none' }}>
-                        <a href="#/incluir">
-                            <i className="fa fa-plus"></i> Incluir
-                        </a>
-                    </li>
-                    <li style={{ color: 'white', textDecoration: 'none', listStyleType: 'none' }}>
-                        <a href="#/alterar">
-                            <i className="fa fa-plus"></i> Alterar
-                        </a>
-                    </li>
-                    <li style={{ color: 'white', textDecoration: 'none', listStyleType: 'none' }}>
-                        <a href="#/excluir">
-                            <i className="fa fa-plus"></i> Excluir
-                        </a>
-                    </li>
-                    <li style={{ color: 'white', textDecoration: 'none', listStyleType: 'none' }}>
-                        <a href="#/listar">
-                            <i className="fa fa-plus"></i> Listar
-                        </a>
-                    </li>
-                </ul>
-            </li>
-
-    </aside>*/
-
     import React, { useState } from 'react';
 
     export default function Menu(props) {
-        const [showUserOptions, setShowUserOptions] = useState(false);
+        const [controlarVisibilidade, setcontrolarVisibilidade] = useState(false);
     
-        const toggleUserOptions = () => {
-            setShowUserOptions(!showUserOptions);
+        const alterarEstado = () => {
+            setcontrolarVisibilidade(!controlarVisibilidade);
         };
     
         return (
@@ -50,10 +13,10 @@ export default props =>
                     <i className="fa fa-home"></i> Início
                 </a>
                 <li style={{ color: 'white', textDecoration: 'none', listStyleType: 'none' }}>
-                    <a href="#/" onClick={toggleUserOptions} style={{ cursor: 'pointer' }}>
+                    <a href="#/" onClick={alterarEstado} style={{ cursor: 'pointer' }}>
                         <i className="fa fa-users"></i> Usuários
                     </a>
-                    {showUserOptions && (
+                    {controlarVisibilidade && (
                         <ul>
                             <li style={{ color: 'white', textDecoration: 'none', listStyleType: 'none' }}>
                                 <a href="#/incluir">
