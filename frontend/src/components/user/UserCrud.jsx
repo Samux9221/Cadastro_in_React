f5cc8e68aff9d2a33dcb6c13c1a18d54c900a030
Samuel Fernandes Silva <169082016+Samux9221@users.noreply.github.com>
2024-10-01 11:18:38 -0600

    Adicionando a função 'incluir'
    ...

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -10,33 +10,15 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
 }
 
-const baseUrl = 'http://localhost:3001/users'
-
-const initialState = {
-    user: {name: '', email: ''},
-    list: []
-}
-
 
 export default class UserCrud extends Component {
 
-    state = { ...initialState }
-
-    clear() {
-         this.setState({ user: initialState.user })
-    }
-
-    save() {
-        const user = this.state.user
-        const method = user.id ? 'put' : 'post'
-        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
-    }
 
     render () {
         return (
             <div>
                 <Main {...headerProps}>
-                    Escolha no menu qual ação deseja executar
+                    Escolha ao lado qual ação deseja realizar
                 </Main>
             </div>
         )
