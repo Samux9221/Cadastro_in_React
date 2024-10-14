5aec3249c8e2006e2a40e258ffbf5c8cc82774e6
Samuel Fernandes Silva <169082016+Samux9221@users.noreply.github.com>
2024-10-14 13:03:53 -0600

    arrumando cadastro de usuarios
    ...

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,26 +1,41 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-
-import Main from "../template/Main"
-
+import React, { Component } from 'react';
+import axios from 'axios';
+import Main from "../template/Main";
+import './Acoes_users.css';
 
 const headerProps = {
     icon: 'users',
     title: 'Usuários',
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
-}
-
+};
 
 export default class UserCrud extends Component {
+    state = {
+        users: []
+    };
 
+    componentDidMount() {
+        axios.get('http://localhost:3000/users')
+            .then(response => {
+                this.setState({ users: response.data });
+            });
+    }
 
-    render () {
+    renderUsers() {
+        return this.state.users.map(user => (
+            <li key={user.id}>{user.name} - {user.email}</li>
+        ));
+    }
+
+    render() {
         return (
             <div>
                 <Main {...headerProps}>
-                    Escolha ao lado qual ação deseja realizar
+                    <ul className="user-list">
+                        {this.renderUsers()}
+                    </ul>
                 </Main>
             </div>
-        )
+        );
     }
 }
